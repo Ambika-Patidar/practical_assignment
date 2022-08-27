bf60f4fbfb9d10f837fa26f784c5b2e3dc8f9b51
Ambika Patidar <ambika.patidar.cse@gmail.com>
2022-08-28 01:57:11 +0530

    Added session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  resources :users, only: :index
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users/sessions#new'
 end
