e53c29f4f90aeb12955782f97c85d9873a21efbb
Ambika Patidar <ambika.patidar.cse@gmail.com>
2022-08-28 12:04:22 +0530

    Added users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum roles: { '0': 'admin', '1': 'user' }
 end
