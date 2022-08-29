c320dec5cb83f35b5efc9797ccf98276cd7b2c36
Ambika Patidar <ambika.patidar.cse@gmail.com>
2022-08-29 19:18:57 +0530

    Set default role

---
+++ db/migrate/20220829134718_set_default_role_value_to_users.rb
@@ -0,0 +1,5 @@
+class SetDefaultRoleValueToUsers < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default :users, :role, 1
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_085413) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_134718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_085413) do
     t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
-    t.integer "role"
+    t.integer "role", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "remember_created_at"
